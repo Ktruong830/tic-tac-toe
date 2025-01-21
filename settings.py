f918c9b93dddd9443efbc68a8a3de8b02d027377
Ktruong830 <truongkhq@gmail.com>
2025-01-20 19:56:28 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,10 @@
+# Represents how many rows and columns the board will have (can be adjusted)
+GRID_SIDE_LENGTH: int = 3
+ROW_AMOUNT: int = GRID_SIDE_LENGTH
+COLUMN_AMOUNT: int = GRID_SIDE_LENGTH
+
+# Determines how many marks are needed in a line to win
+WIN_CONDITION: int = GRID_SIDE_LENGTH
+
+# Holds the turn number
+turnNumber: int = 1
